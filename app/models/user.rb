84d9eb5af73aba9299fe27950ed0e3b151171482
paulotijero <paulotijero@gmail.com>
2019-04-09 17:59:30 -0500

    Add validate inputs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,34 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { in: 2..60 }
+  validates :last_name, presence: true, length: { in: 2..60 }
+  validates :username, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :email, presence: true, uniqueness: true, confirmation: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :email_confirmation, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }, format: { with: /.*[0-9].*/ }
+  validates :password_confirmation, presence: true
+  validates :password_hint, length: { maximum: 100 }
+
+  before_save :validate_name
+
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def validate_name
+    self.first_name = capitalize(self.first_name)
+    self.last_name = capitalize(self.last_name)
+  end
+
+  def capitalize(name)
+    ignore = [
+      "de", 
+      "la", 
+      "del"
+    ]
+    name = name.split(" ").map do |val|
+      ignore.include?(val.downcase) ? val.downcase : val.capitalize
+    end
+    name.join(" ")
+  end
+
 end
